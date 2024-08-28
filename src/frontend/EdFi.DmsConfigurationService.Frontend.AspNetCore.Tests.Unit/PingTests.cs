41dad9d467dde65e1e841651ca93bef7fde03bcd
Adam Hopkins <adam.hopkins@simpat.tech>
2024-08-28 13:00:33 -0500

    Unit test project

---
+++ src/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore.Tests.Unit/PingTests.cs
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+
+using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace EdFi.DmsConfigurationService.Frontend.AspNetCore.Tests.Unit;
+
+[TestFixture]
+public class PingTests
+{
+    [Test]
+    public async Task TestPingEndpoint()
+    {
+        // Arrange
+        await using var factory = new WebApplicationFactory<Program>();
+        using var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/ping");
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        DateTime.TryParse(content, out DateTime dt).Should().Be(true);
+    }
+}
--- src/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore/Program.cs
+++ src/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore/Program.cs
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
@@ -5,6 +10,12 @@ var app = builder.Build();
 app.UseRouting();
 
 app.MapGet("/", () => "DMS Configuration Service");
-app.MapGet("/ping", () => Results.Ok(DateTime.Now));
+app.MapGet("/ping", () => Results.Text(DateTime.Now.ToString()));
 
 app.Run();
+
+public partial class Program
+{
+    // Compliant solution for Sonar lint S1118
+    private Program() { }
+}
