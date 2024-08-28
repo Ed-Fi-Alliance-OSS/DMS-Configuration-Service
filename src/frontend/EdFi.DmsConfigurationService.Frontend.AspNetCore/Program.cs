cc12e00b604baf2e02709b7517a9139b8b9a7db6
Adam Hopkins <adam.hopkins@simpat.tech>
2024-08-28 12:22:22 -0500

    Ping endpoint

--- src/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore/Program.cs
+++ src/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore/Program.cs
@@ -1,32 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseRouting();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapGet("/", () => "DMS Configuration Service");
+app.MapGet("/ping", () => Results.Ok(DateTime.Now));
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
