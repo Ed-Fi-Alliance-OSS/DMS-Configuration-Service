7331747920cbf02837e31b0c19c8e1136be9e15e
Adam Hopkins <adam.hopkins@simpat.tech>
2024-08-28 13:49:20 -0500

    dockerfile

--- src/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore.Tests.Unit/PingTests.cs
+++ src/frontend/EdFi.DmsConfigurationService.Frontend.AspNetCore.Tests.Unit/PingTests.cs
@@ -4,6 +4,11 @@
 // See the LICENSE and NOTICES files in the project root for more information.
 
 
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
 using System.Net;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
