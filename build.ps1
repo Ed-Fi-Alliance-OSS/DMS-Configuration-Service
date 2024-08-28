2f7b03f937158af0488ab22c406766f7c1e8054f
Adam Hopkins <adam.hopkins@simpat.tech>
2024-08-28 16:03:04 -0500

    Change file encoding

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: Apache-2.0
+﻿# SPDX-License-Identifier: Apache-2.0
 # Licensed to the Ed-Fi Alliance under one or more agreements.
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
@@ -271,7 +271,7 @@ function Invoke-TestExecution {
     switch ($Filter) {
         E2ETests { Invoke-Step { E2ETests } }
         UnitTests { Invoke-Step { UnitTests } }
-        IntegrationTests { Invoke-Step { IntegrationTests }}
+        IntegrationTests { Invoke-Step { IntegrationTests } }
         Default { "Unknow Test Type" }
     }
 }
