75f4e088e49d24281ad8911e7218fed96471a579
Johnny Brenes <johnny.brenes@simpat.tech>
2024-10-09 16:16:33 -0600

    Replacing referenced tables with Json data type and renaming db

--- src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0001_Create_Vendor_Table.sql
+++ src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0001_Create_Vendor_Table.sql
@@ -3,16 +3,16 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-CREATE TABLE cms.Vendor (
+CREATE TABLE config.Vendor (
     Id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
     Company VARCHAR(256) NOT NULL,
-    NamespacePrefixes VARCHAR(256),
+    NamespacePrefixes JSONB NOT NULL,
     ContactName VARCHAR(128),
     ContactEmailAddress VARCHAR(320)
 );
 
-COMMENT ON COLUMN cms.Vendor.Id IS 'Vendor or company id';
-COMMENT ON COLUMN cms.Vendor.Company IS 'Vendor or company name';
-COMMENT ON COLUMN cms.Vendor.NamespacePrefixes IS 'Namespace prefix for the vendor. Multiple namespace prefixes can be provided as comma separated list if required';
-COMMENT ON COLUMN cms.Vendor.ContactName IS 'Vendor contact name';
-COMMENT ON COLUMN cms.Vendor.ContactEmailAddress IS 'Vendor contact email id';
+COMMENT ON COLUMN config.Vendor.Id IS 'Vendor or company id';
+COMMENT ON COLUMN config.Vendor.Company IS 'Vendor or company name';
+COMMENT ON COLUMN config.Vendor.NamespacePrefixes IS 'Namespace prefix for the vendor. Multiple namespace prefixes can be provided as comma separated list if required';
+COMMENT ON COLUMN config.Vendor.ContactName IS 'Vendor contact name';
+COMMENT ON COLUMN config.Vendor.ContactEmailAddress IS 'Vendor contact email id';
--- src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0002_Create_Application_Table.sql
+++ src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0002_Create_Application_Table.sql
@@ -3,50 +3,17 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-CREATE TABLE cms.Application (
+CREATE TABLE config.Application (
 	Id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
     ApplicationName VARCHAR(255) NOT NULL,
     VendorId BIGINT NOT NULL,
-    ClaimSetName VARCHAR(255) NOT NULL,   
-    CONSTRAINT fk_vendor FOREIGN KEY (vendorId) REFERENCES cms.Vendor(id) ON DELETE CASCADE
+    ClaimSetName VARCHAR(255) NOT NULL,
+    EducationOrganizations JSONB NOT NULL,
+    CONSTRAINT fk_vendor FOREIGN KEY (VendorId) REFERENCES config.Vendor(Id) ON DELETE CASCADE
 );
 
-COMMENT ON COLUMN cms.Application.Id IS 'Application id';
-COMMENT ON COLUMN cms.Application.ApplicationName IS 'Application name';
-COMMENT ON COLUMN cms.Application.VendorId IS 'Vendor or company id';
-COMMENT ON COLUMN cms.Application.ClaimSetName IS 'Claim set name';
-
-CREATE TABLE cms.ApplicationProfile (
-    ApplicationId BIGINT NOT NULL,
-    ProfileId BIGINT NOT NULL,
-    CONSTRAINT pk_applicationProfile PRIMARY KEY (ApplicationId, ProfileId),
-    CONSTRAINT fk_application FOREIGN KEY (ApplicationId) REFERENCES cms.Application(id) ON DELETE CASCADE
-);
-
-COMMENT ON TABLE cms.ApplicationProfile IS 'Relationship of applications with profiles';
-COMMENT ON COLUMN cms.ApplicationProfile.ApplicationId IS 'Application id';
-COMMENT ON COLUMN cms.ApplicationProfile.ProfileId IS 'Profile id';
-
-CREATE TABLE cms.ApplicationEducationOrganization (
-    ApplicationId BIGINT NOT NULL,
-    EducationOrganizationId BIGINT NOT NULL,
-    CONSTRAINT pk_applicationEducationOrganization PRIMARY KEY (ApplicationId, EducationOrganizationId),
-    CONSTRAINT fk_application_educationOrganization FOREIGN KEY (ApplicationId) REFERENCES cms.Application(id) ON DELETE CASCADE
-);
-
-COMMENT ON TABLE cms.ApplicationEducationOrganization IS 'Relationship of applications with educational organizations';
-COMMENT ON COLUMN cms.ApplicationEducationOrganization.ApplicationId IS 'Application id';
-COMMENT ON COLUMN cms.ApplicationEducationOrganization.EducationOrganizationId IS 'Education organization id';
-
-
--- Crear tabla para almacenar los odsInstanceIds relacionados
-CREATE TABLE cms.ApplicationOdsInstance (
-    ApplicationId BIGINT NOT NULL,
-    OdsInstanceId BIGINT NOT NULL,
-    CONSTRAINT pk_applicationOdsInstance PRIMARY KEY (ApplicationId, OdsInstanceId),
-    CONSTRAINT fk_application_odsInstance FOREIGN KEY (ApplicationId) REFERENCES cms.Application(id) ON DELETE CASCADE
-);
-
-COMMENT ON TABLE cms.ApplicationOdsInstance IS 'Relationship of applications with ODS instances';
-COMMENT ON COLUMN cms.ApplicationOdsInstance.ApplicationId IS 'Application id';
-COMMENT ON COLUMN cms.ApplicationOdsInstance.OdsInstanceId IS 'ODS instance id';
+COMMENT ON COLUMN config.Application.Id IS 'Application id';
+COMMENT ON COLUMN config.Application.ApplicationName IS 'Application name';
+COMMENT ON COLUMN config.Application.VendorId IS 'Vendor or company id';
+COMMENT ON COLUMN config.Application.ClaimSetName IS 'Claim set name';
+COMMENT ON COLUMN config.Application.EducationOrganizations IS 'Education organization ids';
