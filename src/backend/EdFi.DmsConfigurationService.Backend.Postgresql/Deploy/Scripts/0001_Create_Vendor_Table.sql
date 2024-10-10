8bc10ec8daba08a94eaf701e65c024d7c93124e8
Johnny Brenes <johnny.brenes@simpat.tech>
2024-10-10 09:20:20 -0600

    Scripts that make changes to relational tables EdOrgs and Namespces

--- src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0001_Create_Vendor_Table.sql
+++ src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0001_Create_Vendor_Table.sql
@@ -3,16 +3,26 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-CREATE TABLE config.Vendor (
+CREATE TABLE dmscs.Vendor (
     Id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
-    Company VARCHAR(256) NOT NULL,
-    NamespacePrefixes JSONB NOT NULL,
+    Company VARCHAR(256) NOT NULL,    
     ContactName VARCHAR(128),
     ContactEmailAddress VARCHAR(320)
 );
 
-COMMENT ON COLUMN config.Vendor.Id IS 'Vendor or company id';
-COMMENT ON COLUMN config.Vendor.Company IS 'Vendor or company name';
-COMMENT ON COLUMN config.Vendor.NamespacePrefixes IS 'A JSONB array of namespace prefixes associated with the vendor';
-COMMENT ON COLUMN config.Vendor.ContactName IS 'Vendor contact name';
-COMMENT ON COLUMN config.Vendor.ContactEmailAddress IS 'Vendor contact email id';
+COMMENT ON COLUMN dmscs.Vendor.Id IS 'Vendor or company id';
+COMMENT ON COLUMN dmscs.Vendor.Company IS 'Vendor or company name';
+COMMENT ON COLUMN dmscs.Vendor.ContactName IS 'Vendor contact name';
+COMMENT ON COLUMN dmscs.Vendor.ContactEmailAddress IS 'Vendor contact email id';
+
+
+CREATE TABLE dmscs.VendorNamespacePrefix (
+    VendorId BIGINT NOT NULL,
+    NamespacePrefixes VARCHAR(128) NOT NULL,
+    CONSTRAINT pk_VendorNamespacePrefix PRIMARY KEY (VendorId, NamespacePrefixes),
+    CONSTRAINT fk_vendor_NamespacePrefixes FOREIGN KEY (VendorId) REFERENCES dmscs.Vendor(id) ON DELETE CASCADE
+);
+
+COMMENT ON TABLE dmscs.VendorNamespacePrefix IS 'Relationship of vendors with Namespace prefix';
+COMMENT ON COLUMN dmscs.VendorNamespacePrefix.VendorId IS 'Vendor or company id';
+COMMENT ON COLUMN dmscs.VendorNamespacePrefix.NamespacePrefixes IS 'Namespace prefix for the vendor';
--- src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0002_Create_Application_Table.sql
+++ src/backend/EdFi.DmsConfigurationService.Backend.Postgresql/Deploy/Scripts/0002_Create_Application_Table.sql
@@ -3,17 +3,26 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-CREATE TABLE config.Application (
-	Id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+CREATE TABLE dmscs.Application (
+    Id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
     ApplicationName VARCHAR(255) NOT NULL,
     VendorId BIGINT NOT NULL,
     ClaimSetName VARCHAR(255) NOT NULL,
-    EducationOrganizations JSONB NOT NULL,
-    CONSTRAINT fk_vendor FOREIGN KEY (VendorId) REFERENCES config.Vendor(Id) ON DELETE CASCADE
+    CONSTRAINT fk_vendor FOREIGN KEY (VendorId) REFERENCES dmscs.Vendor(Id) ON DELETE CASCADE
 );
 
-COMMENT ON COLUMN config.Application.Id IS 'Application id';
-COMMENT ON COLUMN config.Application.ApplicationName IS 'Application name';
-COMMENT ON COLUMN config.Application.VendorId IS 'Vendor or company id';
-COMMENT ON COLUMN config.Application.ClaimSetName IS 'Claim set name';
-COMMENT ON COLUMN config.Application.EducationOrganizations IS 'A JSONB array of Education organization ids';
+COMMENT ON COLUMN dmscs.Application.Id IS 'Application id';
+COMMENT ON COLUMN dmscs.Application.ApplicationName IS 'Application name';
+COMMENT ON COLUMN dmscs.Application.VendorId IS 'Vendor or company id';
+COMMENT ON COLUMN dmscs.Application.ClaimSetName IS 'Claim set name';
+
+CREATE TABLE dmscs.ApplicationEducationOrganization (
+    ApplicationId BIGINT NOT NULL,
+    EducationOrganizationId BIGINT NOT NULL,
+    CONSTRAINT pk_applicationEducationOrganization PRIMARY KEY (ApplicationId, EducationOrganizationId),
+    CONSTRAINT fk_application_educationOrganization FOREIGN KEY (ApplicationId) REFERENCES dmscs.Application(id) ON DELETE CASCADE
+);
+
+COMMENT ON TABLE dmscs.ApplicationEducationOrganization IS 'Relationship of applications with educational organizations';
+COMMENT ON COLUMN dmscs.ApplicationEducationOrganization.ApplicationId IS 'Application id';
+COMMENT ON COLUMN dmscs.ApplicationEducationOrganization.EducationOrganizationId IS 'Education organization id';
